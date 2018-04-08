e7e8504ad8d5b839036be14e0f157130fe92eacb
UTamaD <37644187+UTamaD@users.noreply.github.com>
2018-04-08 22:26:21 +0900

    Add files via upload

---
+++ lineArt_1/lineArt_1.js
@@ -0,0 +1,25 @@
+var i=0;
+var j=0;
+var z=0;
+
+
+function setup() {
+  background(255);
+  createCanvas(512,512)
+  frameRate(60);
+}
+
+function draw() { 
+
+  if (i<width) {
+    i=i+3;
+    stroke(0);
+    line(0, i, i, height);
+    line(width-0, height-i, width-i, height-height);
+    line(0, i, width-i, 0);
+    line(width-0, height-i, i, height);
+    line(i, 0, width-i, height);
+    line(width, height-i, 0, i);
+
+  }
+}
