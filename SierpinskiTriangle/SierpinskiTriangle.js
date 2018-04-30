28307a7d2bc4c2c05996b2fec0be08a3f75d04df
UTamaD <37644187+UTamaD@users.noreply.github.com>
2018-04-30 13:49:59 +0900

    Add files via upload

---
+++ SierpinskiTriangle/SierpinskiTriangle.js
@@ -0,0 +1,29 @@
+
+function makeTri(var x1, var y1, var x2, var y2, var x3, var y3, var N) {
+  if (N!=0) { 
+    triangle((x1+x3)/2, (y1+y3)/2, (x1+x3)/2-(x2-x1)/2, (y1+y3)/2, x1, y3);
+    triangle((x2+x3)/2, (y2+y3)/2, (x2+x3)/2+(x2-x1)/2, (y2+y3)/2, x2, y3);
+    triangle(x1+(x2-x1)/4, y2-(y3-y2)/2, x1+(x2-x1)*3/4, y2-(y3-y2)/2, (x1+x2)/2, y2);
+    makeTri((x2+x3)/2, (y2+y3)/2, (x2+x3)/2+(x2-x1)/2, (y2+y3)/2, x2, y3, N-1);
+    makeTri(x1+(x2-x1)/4, y2-(y3-y2)/2, x1+(x2-x1)*3/4, y2-(y3-y2)/2, (x1+x2)/2, y2, N-1);
+    makeTri((x1+x3)/2, (y1+y3)/2, (x1+x3)/2-(x2-x1)/2, (y1+y3)/2, x1, y3, N-1);
+
+  }
+}
+var M=2;
+function setup() {
+  createCanvas(1000, 1000);
+  background(255);
+  noFill();
+}
+
+function draw() {
+  background(255);
+  triangle(width/2, 0, 0, height, width, height);
+  triangle(width/4, height/2, width*3/4, height/2, width/2, height);
+  makeTri(width/4, height/2, width*3/4, height/2, width/2, height, M);
+}
+
+function mousePressed() {
+  M++;
+}
