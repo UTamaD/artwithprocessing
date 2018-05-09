11699506f9518d1081463e0a399bf0cc99ce412b
UTamaD <37644187+UTamaD@users.noreply.github.com>
2018-05-09 16:30:38 +0900

    Update SierpinskiTriangle.js

--- SierpinskiTriangle/SierpinskiTriangle.js
+++ SierpinskiTriangle/SierpinskiTriangle.js
@@ -1,6 +1,6 @@
 
 
-function makeTri(var x1, var y1, var x2, var y2, var x3, var y3, var N) {
+function makeTri( x1,  y1,  x2,  y2,  x3,  y3,  N) {
   if (N!=0) { 
     triangle((x1+x3)/2, (y1+y3)/2, (x1+x3)/2-(x2-x1)/2, (y1+y3)/2, x1, y3);
     triangle((x2+x3)/2, (y2+y3)/2, (x2+x3)/2+(x2-x1)/2, (y2+y3)/2, x2, y3);
@@ -26,8 +26,8 @@ function draw() {
   triangle(width/4, height/2, width*3/4, height/2, width/2, height);
   makeTri(width/4, height/2, width*3/4, height/2, width/2, height, M);
 }
-/*
+
 function mousePressed() {
   M++;
-}*/
+}
 
